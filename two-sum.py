c5229bf68633bfb8f4f021ca08bbd4f19c7138e8
Tripti Gupta <triptigupta346@gmail.com>
2020-09-21 18:55:12 +0530

    Add files via upload

---
+++ two-sum.py
@@ -0,0 +1,27 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+# Example 1:
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+ 
+def find_two_sum(nums,target):
+    prevMap = {}  # val : index
+    for i, n in enumerate(nums):
+        diff = target - n
+        if diff in prevMap:
+            return [prevMap[diff], i]
+        prevMap[n] = i
+    return
+
+if __name__ == '__main__':
+    ls = [2,7,11,15];
+    print(find_two_sum(ls,9));
